a41b6561f793ba2d262e37a9bd9f0d69dacda3d8
magnus <magnus.ja.gustafsson@gmail.com>
2019-06-12 19:19:18 +0200

    added calculate_position function

--- src/distance.c
+++ src/distance.c
@@ -66,3 +66,28 @@ uint32_t calculate_distance() {
 	printf("distance: %ldcm, q: %d\n", distance, q);
 	return distance;
 }
+
+Position_t calculate_position(uint32_t x_dist, uint32_t y_dist, uint32_t o_dist) {
+    uint32_t area, p, y_pos, x_pos;
+    Position_t pos;
+    p = (x_dist + x_size + o_dist) / 2;
+    area = sqrt(p*(p-a)*(p-b)*(p-c));
+    pos.yPos = 2 * area / x_size ; 
+    pos.xPos = sqrt(o_dist^2 - y_pos^2);
+    return pos;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- src/distance.h
+++ src/distance.h
@@ -2,10 +2,11 @@
 #define DISTANCE_H_
 
 #include "settings.h"
-
+#include <math.h>
 void store_RSSI_value(int rssi_value);
 void calculate_RSSI_average();
 uint32_t calculate_distance();
 void set_axis(uint32_t x, uint32_t y);
+Position_t calculate_position(uint32_t x_dist, uint32_t y_dist, uint32_t o_dist);
 
 #endif
